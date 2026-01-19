74b988011d50ff62cd4d6aada133dc2b360e4775
Matt Proud <mproud@mattproud.com>
2026-01-19 16:36:48 -0500

    Fixes

--- frontend/app/api/reports/pdf/route.ts
+++ frontend/app/api/reports/pdf/route.ts
@@ -1,30 +1,34 @@
-import puppeteer from "@cloudflare/puppeteer"
+// import puppeteer from "@cloudflare/puppeteer"
 import { NextResponse } from "next/server"
-import { getCloudflareContext } from "@opennextjs/cloudflare"
+// import { getCloudflareContext } from "@opennextjs/cloudflare"
 
-// export const runtime = "edge"
+// // export const runtime = "edge"
 
-type Env = {
-    BROWSER: any
-}
+// type Env = {
+//     BROWSER: any
+// }
 
-export async function POST( request: Request ) {
-    const { env } = getCloudflareContext()
+// export async function POST( request: Request ) {
+//     const { env } = getCloudflareContext()
 
-    const cfEnv = env as unknown as Env
+//     const cfEnv = env as unknown as Env
     
-    const browser = await puppeteer.launch( cfEnv.BROWSER )
-    const page = await browser.newPage()
+//     const browser = await puppeteer.launch( cfEnv.BROWSER )
+//     const page = await browser.newPage()
 
-    try {
-        const document = "<h1>Hello world!</h1><p>Test</p>"
-    } catch( error ) {
+//     try {
+//         const document = "<h1>Hello world!</h1><p>Test</p>"
+//     } catch( error ) {
 
-    } finally {
-        // Make sure resources are cleaned up
-        await page.close().catch(() => {})
-        await browser.close().catch(() => {})
-    }
+//     } finally {
+//         // Make sure resources are cleaned up
+//         await page.close().catch(() => {})
+//         await browser.close().catch(() => {})
+//     }
 
+//     return NextResponse.json({ ok: 'ok' })
+// }
+
+export async function POST() {
     return NextResponse.json({ ok: 'ok' })
 }
